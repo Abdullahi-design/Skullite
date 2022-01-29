1c33dc7064edea1ffc6b338d490c665cb87af884
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-01-29 13:45:08 +0100

    The new skullite

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+<div id="parrent div" class="h-screen bg-gray-100">
+  <article class="bg-green-400 text-sm text-gray-800 p-4 text-center tracking-widest">
+    <h4>Skullite Needs your Support!</h4>
+  </article>
+  <nav class="flex fixed justify-between w-full bg-transparent p-8">    
+    <div id="logo" class="flex">
+      <img src="https://img.search.brave.com/6ByFyP38xZHtwVNcHczl52b_QQ0BHV2ZIcFTMq0-7sQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a2t6TGExS3FQLTdC/Q0hsWFFFN29RSGFI/YSZwaWQ9QXBp" alt="logo here" class="w-10 rounded-full">
+      <span class="p-2 text-gray-800 text-lg">Skullite</span> 
+    </div>
+    <button type="button" class="tracking-widest inline-block px-6 py-3 bg-green-600 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Price</button>  
+  </nav>
+  <div class="p-20"></div>  
+  <div id="wrapper">
+    <div id="logo" class="">
+      <img src="https://img.search.brave.com/6ByFyP38xZHtwVNcHczl52b_QQ0BHV2ZIcFTMq0-7sQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a2t6TGExS3FQLTdC/Q0hsWFFFN29RSGFI/YSZwaWQ9QXBp" alt="logo here" class="w-44 rounded-full">
+      <p class=" text-right pr-20 text-gray-800 text-2xl tracking-wider">
+        Skullite is a decentralized token focused on revolutionizing WEB 3.0 <br>
+        <span>It is built to enable scalable, user-friendly apps for the world.</span>
+      </p> 
+    </div>
+    <header class="text-green-400 pt-32 pb-12 text-center text-2xl tracking-widest font-extrabold">Get Started With Skullite</header>
+    <div class="bg-gray-100 flex justify-around ">      
+      <article class="leading-8 text-lg text-gray-700 hover:text-gray-900">Skullite uses peer-to-peer technology to operate with <br>
+       no central authority or banks; managing transactions <br>
+        and the issuing of bitcoins is carried out collectively <br>
+         by the network. <span class="font-semibold">Skullite is open-source; its design is <br>
+          public, nobody owns or controls Skullite</span>  and <br>
+           everyone can take part. Through many of its unique <br>
+            properties, Bitcoin allows exciting uses that could not <br>
+             be covered by any previous payment system.
+      </article>
+      <div class="">
+        <img src="https://img.search.brave.com/zkcQO6wTx8Y-ydSelJu048aWWF4tGSH5Ny6IvZu54js/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5w/TnpySnp5cWhpdXlB/UkxRX2VsMUtRSGFI/YSZwaWQ9QXBp" alt="" class="w-44">
+      </div>      
+    </div>
+    <div class="bg-gray-100 text-center py-32">
+        <button type="button" class="tracking-widest inline-block px-6 py-5 bg-green-600 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Get Started With Skullite</button>
+    </div>
+  </div>
+  <div id="footer" class="text-green-400 bg-gray-900 flex justify-around w-full  h-64 py-20">
+    <div>
+      <div class="">
+        <span class="pr-8">Support Skullite </span>
+        <button type="button" class="tracking-widest inline-block px-6 py-3 bg-green-600 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Donate</button>
+      </div>
+      <a href="0xb5E31Ba447734081e5F82EA92979143bafdBabeF" class=" text-blue-600 hover:text-blue-700 hover:underline">0xb5E31Ba447734081e5F82EA92979143bafdBabeF</a>
+    </div>
+    <div>
+      <header>Contact</header>
+      <li>Learn about Web 3.0</li>
+      <li>What is decentralized token?</li>
+      <li>open-source</li>
+    </div>
+    <div>
+      <header>FAQ</header>
+      <li>Learn about Web 3.0</li>
+      <li>What is decentralized token?</li>
+      <li>open-source</li>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
